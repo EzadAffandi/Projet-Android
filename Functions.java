dd4bcd7a7523dae36c136e01e6717cd7552133ab
Adam Bin Azmi <92119571+adamazm@users.noreply.github.com>
2022-03-26 17:37:22 +0100

    Add files via upload

--- Functions.java
+++ Functions.java
@@ -1,45 +1,119 @@
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
+package com.example.but011;
 
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Functions {
-    public static String[] parseListeBUT(String input) {
+    private static int id;
+    private static int idCompetence;
+    private static int idUe;
+    private static String specialite;
+    private static String parcours;
+    private static String nom;
+    private static String cours; //it is hours but in string
+    private static String td; //it is hours but in string
+    private static String tp; //it is hours but in string
+    private static String description;
+    private static String projet; //it is hours but in string
 
-        JSONArray jArray;
-        JSONObject jObject;
-        String[] listeBUT = {};
-
+    public static int getId(JSONObject jObject) {
         try {
-            jArray= new JSONArray(input);
-            for (int i= 0; i< jArray.length(); i++) {
-                jObject= jArray.getJSONObject(i);
-                listeBUT[i]= jObject.getString("specialite");
-            }
-
-
+            id = jObject.getInt("id");
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return listeBUT;
+        return id;
+    }
+
+    public static String getSpecialite(JSONObject jObject) {
+        try {
+            specialite = jObject.getString("specialite");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return specialite;
+    }
+
+    private static int getIdUe(JSONObject jObject) {
+        idUe = jObject.getInt("Id")
+    }
+
+    public static int getIdCompetence(JSONObject jObject) {
+        try {
+            idCompetence = jObject.getInt("idCompetence");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return idCompetence;
+    }
+
+    public static String getSpecialite(JSONObject jObject) {
+        try {
+            specialite = jObject.getString("specialite");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return specialite;
+    }
+    public static String getParcours(JSONObject jObject) {
+        try {
+            parcours = jObject.getString("parcours");
+            if (parcours == "null") {
+                parcours = "Tronc Commun";
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return parcours;
+    }
+    public static String getNom(JSONObject jObject) {
+        try {
+            nom = jObject.getString("nom");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return nom;
+    }
+    public static String getCours(JSONObject jObject) {
+        try {
+            cours = jObject.getString("cours");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return cours;
+    }
+    public static String getTd(JSONObject jObject) {
+        try  {
+            td = jObject.getString("td"); 
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return td;
+    }
+    public static String getTp(JSONObject jObject) {
+        try {
+            tp = jObject.getString("tp");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return tp;
+    }
+    public static String getDescription(JSONObject jObject) {
+        try {
+            description = jObject.getString("description");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return description;
+    }
+    public static String getProjet(JSONObject jObject) {
+        try {
+            projet = jObject.getString("projet");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return projet;
     }
     
-     public static int[] parseBUTId(String input) {
-        JSONArray jArray;
-        JSONObject jObject;
-        int[] idArray = {};
-
-        try {
-            jArray = new JSONArray(input);
-            for (int i = 0; i < jArray.length(); i++) {
-                jObject = jArray.getJSONObject(i);
-                idArray[i] = jObject.getInt("id");
-
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return idArray;
-    }
-
 }
